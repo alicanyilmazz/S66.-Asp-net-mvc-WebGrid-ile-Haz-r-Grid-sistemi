8ce9222d9275987d372c2b9e3a4577822cc35360
alicanyilmazz <alicanyilmazz@users.noreply.github.com>
2019-04-29 16:47:14 +0300

    Add project files.

---
+++ WebGridUsing/Controllers/MyWebGridController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebGridUsing.Models;
+
+namespace WebGridUsing.Controllers
+{
+    public class MyWebGridController : Controller
+    {
+        // GET: MyWebGrid
+        public ActionResult Index()
+        {
+            Book.GenerateFakeData(50);
+
+            return View(Book._books);
+        }
+
+        [HttpPost]
+        public ActionResult Index(string searchid)
+        {
+
+            List<Book> filtered_data = Book._books.Where(x =>
+              x.Name.ToLower().Contains(searchid) ||
+              x.Author.ToLower().Contains(searchid) ||
+              x.PublishingDate.ToString().Contains(searchid) ||
+              x.price.ToString().Contains(searchid)).ToList();
+
+            return View(filtered_data);
+        }
+    }
+}
+
+//Site.css birtakım kodlar yazarak ekstra düzenlemelerde bulundum.
---
+++ WebGridUsing/Models/Book.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebGridUsing.Models
+{
+    public class Book
+    {
+        public static List<Book> _books { get; set; }
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public DateTime PublishingDate { get; set; }
+        public decimal price { get; set; }
+
+        protected Book()
+        {
+
+        }
+        
+        public static  void GenerateFakeData(int count)
+        {
+            if(_books==null)
+            {
+                _books = GetFakeData(count);
+            }
+        }
+
+        private static List<Book> GetFakeData(int count)
+        {
+            List<Book> _book_list = new List<Book>();
+
+            for (int i = 0; i < count; i++)
+            {
+                _book_list.Add(new Book()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = FakeData.NameData.GetCompanyName(),
+                    Author = FakeData.NameData.GetFullName(),
+                    price = FakeData.NumberData.GetNumber(10, 75),
+                    PublishingDate = FakeData.DateTimeData.GetDatetime(DateTime.Now.AddYears(2), DateTime.Now)
+
+                });
+            }
+
+            return _book_list;
+        }
+    }
+}
